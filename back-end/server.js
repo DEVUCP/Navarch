8b46ec99eab42337bc5ce2ffee466e6904d50231
DEVUCP <OMHN05@protonmail.com>
2025-03-06 05:40:27 +0200

    feat: Add API call to start and stop the minecraft server

--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,77 @@
 const express = require('express');
+const { spawn } = require('child_process');
 const app = express();
 const fs = require('fs');
+const os = require("os");
+
+const serverDirectory = "../server"
+const serverName = "server.jar"
+const serverLogsFilePath = `${serverDirectory}/server_output.txt`
+
+
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
+    console.log(Math.floor(os.freemem()/1000000));
 });
 
-app.get('/start', (req, res) => {
-    res.send('<h1>Hello, Express.js Server!</h1>');
-});
+
 
 app.get('/download/:version', async (req, res) => {
     await downloadServerFiles(req.params.version);
     res.status(201)
 });
 
+let serverProcess = null;
 
+// Start route
+app.get('/start', (req, res) => {
+    if (serverProcess) {
+        return res.status(400).send('Server is already running.');
+    }
+
+    // Define the shell command you want to run
+    const command = 'java';
+    const args = ['-Xmx1024M', '-Xms1024M', '-jar', serverName, 'nogui'];
+
+    // Execute the command
+    serverProcess = spawn(command, args, { cwd: '../server' });
+
+
+    fs.writeFileSync(serverLogsFilePath,"")
+
+    // Log output to the console (optional)
+    serverProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        fs.appendFileSync(serverLogsFilePath, data);
+    });
+
+    serverProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    // Handle process exit
+    serverProcess.on('close', (code) => {
+        console.log(`Server process exited with code ${code}`);
+        serverProcess = null; // Reset the process variable
+    });
+
+    // Respond immediately
+    res.send('Server started.');
+});
+
+// Stop route
+app.get('/stop', (req, res) => {
+    if (!serverProcess) {
+        return res.status(400).send('Server is not running.');
+    }
+
+    // Kill the process
+    serverProcess.stdin.write('stop\n');
+
+    // Respond immediately
+    res.send('Server stopped.');
+});
 
 const port = 3001;
 app.listen(port, () => {
@@ -35,7 +91,7 @@ async function downloadServerFiles(version){
         console.log(response)
         
         if (response.ok) {
-            const fileName = `../server/paper-${version}-${build}.jar`;
+            const fileName = serverDirectory + serverName;
             const arrayBuffer = await response.arrayBuffer();
             const buffer = Buffer.from(arrayBuffer);
             fs.writeFileSync(fileName, buffer);
@@ -51,7 +107,7 @@ async function downloadServerFiles(version){
 }
 
 async function fetchLatestBuild(version){
-    const response = await fetch(`https://api.papermc.io/v2/projects/paper/versions/${version}/builds`)
-    const jsonBuildData = await response.json()
+    const response = await fetch(`https://api.papermc.io/v2/projects/paper/versions/${version}/builds`);
+    const jsonBuildData = await response.json();
     return jsonBuildData.builds.at(-1).build;
 }
