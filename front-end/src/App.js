3216a1bd04ac82dae708b763a018e4f8e173917c
DEVUCP <OMHN05@protonmail.com>
2025-03-08 08:28:13 +0200

    feat: Create and add StartStopBtn component

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import VersionSelectDropdown from './VersionSelectDropdown';
+import StartStopBtn from './StartStopBtn';
 
 function App() {
   return (
@@ -9,8 +10,7 @@ function App() {
     <div className="container">
         <div className="sub-container">
             <h1 className="server-name">ServerName</h1>
-            <button type="button" className="btn btn-success w-100" id="start-stop-btn">Start Server</button>
-            {/* <button type="button" class="btn btn-primary w-100" id="download-server">Download server files<select class="w-100"></select></button> */}
+            <StartStopBtn></StartStopBtn>
           <VersionSelectDropdown></VersionSelectDropdown>
         </div>
     </div>
---
+++ front-end/src/StartStopBtn.js
@@ -0,0 +1,36 @@
+    import { useEffect, useState } from "react";
+    import { useServerStatus } from "./monitor";
+
+    function StartStopBtn(){
+        
+        const [serverStatus, setServerStatus] = useState(useServerStatus());
+        
+
+        async function startStopServer(){
+            try{
+            if(serverStatus){
+                await fetch("http://localhost:3001/server/stop")
+                setServerStatus(false);    
+            }
+            else{
+                await fetch("http://localhost:3001/server/start")
+                setServerStatus(true);  
+            }
+            }
+            catch(error){
+                console.error(error);
+            }
+        }
+        
+        return(<button 
+            type="button" 
+            onClick={startStopServer} 
+            className={"btn " + "btn-"+ (!serverStatus ? "success" : "danger") +" w-100"} 
+            id="start-stop-btn">
+                {!serverStatus ? "Start " : "Stop " +"Server"}
+            </button>
+    )
+    }
+
+
+    export default StartStopBtn;
