fc13249c3704a5609c2a136592cae08745b99e6f
DEVUCP <OMHN05@protonmail.com>
2025-03-09 06:42:47 +0200

    feat: Add quality of life changes to downloadServerFiles button with confirmation modal and disable on download

--- front-end/src/VersionSelectDropdown.js
+++ front-end/src/VersionSelectDropdown.js
@@ -5,7 +5,11 @@ function VersionSelectDropdown(){
     
     const [versionList, setVersionList] = useState([]);
     const [selectedVersion, setSelectedVersion] = useState("1.21.4");
-    
+    const [alertMessage, setAlertMessage] = useState(false);
+    const [downloadStatus, setDownloadStatus] = useState(false);
+    const [disableDownloadBtn, setDisableDownloadBtn] = useState(false);
+
+
     useEffect(() => {
         const fetchVersions = async () => {
             try{
@@ -20,18 +24,92 @@ function VersionSelectDropdown(){
         fetchVersions();
     },[]);
 
+    async function checkServerFilesExist() {
+        try{
+            const response = await fetch(`http://localhost:3001/server/files/check-exist`)
+            const filesExist = await response.json()
+            console.log(filesExist)
+            return filesExist;
+
+        }catch(error){
+            console.error(error)
+        }
+    }
+
+    async function downloadServerFiles(version) {
+        
+        setDisableDownloadBtn(true);        
+        try{
+            const response = await fetch(`http://localhost:3001/server/download/${version}`)
+            if(response.ok){
+                setDownloadStatus(true)
+                setAlertMessage("Server Files Successfully Downloaded")
+                setDisableDownloadBtn(false);        
+            }else{
+                switch (response.status){
+                    case 409:
+                        setDownloadStatus(false)
+                        setAlertMessage("Too many requests, please wait for download to complete")
+                        setDisableDownloadBtn(false);        
+
+                        break;
+                    case 500:
+                        setDownloadStatus(false)
+                        setAlertMessage("Error while trying to download server files...")
+                        setDisableDownloadBtn(false);        
+                        break;
+                    default:
+                        console.log("huhhhhh")
+                    }
+                }
+            }
+        catch(error){
+            console.error("no...")
+        }
+    }
+
+
     return (
-    <div className="btn-group">
-    
-        <button className="btn btn-primary btn-sm" type="button" onClick={() => downloadServerFiles(selectedVersion)}>
+        <>
+        <div className="modal fade" id="fileExistModal" tabIndex="-1" aria-labelledby="FileExistLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h1 className="modal-title fs-5" id="FileExistLabel">Warning!</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              Files already exist, are you sure you want to continue?
+            </div>
+            <div className="modal-footer">
+                <button className="btn btn-secondary" type="button" data-bs-dismiss="modal">
+                    Cancel download
+                </button>
+                <button className="btn btn-success" type="button" data-bs-dismiss="modal" onClick={() => downloadServerFiles(selectedVersion)}>
+                    Proceed to download
+                </button>
+            </div>
+            </div>
+        </div>
+        </div>
+        {alertMessage && (
+            <div 
+            className={"alert "+"alert-" + (downloadStatus ? "success" : "danger")}
+            role="alert"
+            >
+                <button type="button" className="btn-close" onClick={() => setAlertMessage("")} aria-label="Close"></button>
+                {alertMessage}
+            </div>
+        )}
+        <div className="btn-group" role="group">
+        <button type="button" className="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#fileExistModal" disabled={disableDownloadBtn}>
             Download Server Files
         </button>
 
         <div className="dropdown">
-        <button type="button" class="btn btn-sm btn-success dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+        <button type="button" className="btn btn-lg btn-success dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
             <span>{selectedVersion} </span>
         </button>
-
         <ul 
         className={`dropdown-menu ${styles.scrollableDropdown}`}
         >
@@ -44,18 +122,12 @@ function VersionSelectDropdown(){
             ))}
         </ul>
         </div>
-    </div>
+        </div>
+    </>
   );
   }
 
   export default VersionSelectDropdown;
 
 
-async function downloadServerFiles(version) {
-    try{
-        const response = await fetch(`http://localhost:3001/server/download/${version}`)
-    }
-    catch(error){
-        console.error("no...")
-    }
-}
+ 
