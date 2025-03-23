5022f34651bfa0bca9b1675bbb887e9470e44df5
DEVUCP <OMHN05@protonmail.com>
2025-03-23 11:10:22 +0200

    feat: Update server.js to start-up cleaner

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const os = require('os');
+const configUtils = require('./utils/configUtils')
 const serverRoutes = require('./routes/serverRoutes'); // Routes are separated
 const propertiesRoute = require('./routes/propertiesRoutes'); // Routes are separated
 const installationsRoutes = require('./routes/installationsRoutes'); // Routes are separated
-
+const networkingUtils = require('./utils/networkingUtils');
 
 const app = express();
 const port = 3001;
@@ -20,12 +19,21 @@ app.use('/installations', installationsRoutes);
 app.use('/properties', propertiesRoute);
 
 // Default route
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
-    console.log(`Free memory: ${Math.floor(os.freemem() / 1000000)} MB`);
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(port, async () => {
+    console.log(`port: ${port}`);
+    if (!configUtils.doesConfigExist()){
+        configUtils.generateConfigFile();
+        console.log("sever-config generated successfully")
+    }
+    const ip = await networkingUtils.getIP(local=true)
+    console.log("local-ip:", ip);
+    console.log(`attempting to forward ports ${port}, 3000`);
+    networkingUtils.forwardPort(3000, ip);
+    networkingUtils.forwardPort(3001, ip);
+
 });
