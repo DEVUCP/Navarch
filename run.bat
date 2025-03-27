d202c77165d99a1eab1f0f2bed43645aae5879f6
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2025-03-28 00:45:36 +0200

    fix(scripts): Call npm_install incase of missing npm_modules

--- run.bat
+++ run.bat
@@ -4,9 +4,12 @@ echo Running setup scripts...
 
 cd scripts
 call install_dependencies.bat
+
+call npm_install.bat
+
+cd scripts
 call start_servers.bat
 cd ..
 
 echo All scripts completed successfully
-
 pause
---
+++ scripts/npm_install.bat
@@ -0,0 +1,17 @@
+cd ..
+
+if not exist "front-end\node_modules" (
+    echo Installing front-end dependencies...
+    cd front-end
+    call npm install
+    cd ..
+)
+
+if not exist "back-end\node_modules" (
+    echo Installing back-end dependencies...
+    cd back-end
+    call npm install
+    cd ..
+)
+
+cd scripts
