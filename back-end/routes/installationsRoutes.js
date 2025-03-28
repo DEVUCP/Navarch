e194159b4990d2cf6719dc21e7521e7208b5faf0
DEVUCP <OMHN05@protonmail.com>
2025-03-28 20:10:05 +0200

    feat(api): update config with new version and platform from download endpoint call

--- back-end/routes/installationsRoutes.js
+++ back-end/routes/installationsRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const installationsUtils = require('../utils/installationsUtils');
+const { updateConfigAttribute } = require('../utils/configUtils');
 
 const { Sema } = require('async-sema');
 
@@ -12,6 +13,10 @@ router.put('/download/:platform/:version', async (req, res) => {
     try {
         await installationsUtils.downloadRouter(req.params.platform, req.params.version);
         res.status(201).send('Downloaded Successfully');
+
+        updateConfigAttribute("platform", req.params.platform);
+        updateConfigAttribute("version", req.params.version);
+
     } catch (error) {
 
         res.status(500).send(`Error downloading server files: ${error}`);
