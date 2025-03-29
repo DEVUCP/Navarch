58e3c50f89cd37584c2376023758291f95de750e
DEVUCP <OMHN05@protonmail.com>
2025-03-29 17:50:45 +0200

    refactor(backend)!: Move server related json methods to standalone utils module

--- back-end/utils/propertiesUtils.js
+++ back-end/utils/propertiesUtils.js
@@ -2,7 +2,6 @@ const consts = require("../consts");
 const fs = require("fs");
 const { getConfigAttribute } = require("./configUtils");
 const { spawn } = require('child_process');
-const crypto = require('crypto');
 
 async function getProperties(){
     try{
@@ -17,130 +16,6 @@ async function getProperties(){
         console.error(error);
     }
 }
-
-function getWhitelistJSON(){
-    try {
-        if(!fs.existsSync(consts.serverWhitelistPath)){
-            return {};
-        }
-        var whitelistJSON = JSON.parse(fs.readFileSync(consts.serverWhitelistPath,{ encoding: 'utf8', flag: 'r' }));
-        return whitelistJSON;
-
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-function getOpsJSON(){
-    try {
-        if(!fs.existsSync(consts.serverOpsPath)){
-            return {};
-        }
-        var opsJSON = JSON.parse(fs.readFileSync(consts.serverOpsPath,{ encoding: 'utf8', flag: 'r' }));
-        return opsJSON;
-
-    } catch (error) {
-        console.error(error);
-    }  
-}
-
-async function modifyOpsJSON(playerName, add){
-    var opsJSON = getOpsJSON();
-
-    const uuid = await getUUID(playerName);
-    if(add){
-        for(let i = 0; i < opsJSON.length ; i++){
-            if(opsJSON[i].uuid == uuid){
-                throw new Error(`Player ${playerName} already an Operator`);
-            }
-        }
-        opsJSON.push(
-        {
-            "uuid": uuid,
-            "name": playerName,
-            "level": 4,
-            "bypassesPlayerLimit": false
-            }
-        )
-    } else {
-        let duplicateExist = true;
-        for(let i = 0; i < opsJSON.length ; i++){
-            if(opsJSON[i].uuid == uuid){
-                duplicateExist = false;
-                opsJSON.splice(i, 1);
-                break;
-            }
-        }
-        if(duplicateExist)
-            throw new Error(`Player ${playerName} is not an Operator`);
-    }
-
-    fs.writeFileSync(consts.serverOpsPath, JSON.stringify(opsJSON, null, 4), {encoding : 'utf8'});
-
-}
-
-async function getUUID(username) {
-    var uuid = await getMojangUUID(username);
-    console.log(uuid);
-    try {
-        if(uuid === ''){
-            console.log("UUID not found, generating one...")
-            uuid = generateOfflineUUID(username);
-        }
-        return uuid;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-async function getMojangUUID(username) {
-    try {
-        const playerInfo = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
-        const playerInfoJSON = await playerInfo.json();
-        
-        if(!playerInfoJSON)
-            return "";
-        else
-            return `${playerInfoJSON["id"].slice(0, 8)}-${playerInfoJSON["id"].slice(8, 12)}-${playerInfoJSON["id"].slice(12, 16)}-${playerInfoJSON["id"].slice(16, 20)}-${playerInfoJSON["id"].slice(20)}`;
-        
-    } catch (error) {
-        console.error(error);
-    } 
-}
-
-async function generateOfflineUUID(username) {
-
-    
-    try {
-        const data = `OfflinePlayer:${username}`;
-        const hash = crypto.createHash('md5').update(data, 'utf8').digest();
-        
-        // Convert to UUID format (version 3)
-        hash[6] = (hash[6] & 0x0f) | 0x30; // Set version to 3
-        hash[8] = (hash[8] & 0x3f) | 0x80; // Set variant to RFC 4122
-        
-        const hex = hash.toString('hex');
-
-        return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
-    } catch (error) {
-        console.error(error);
-    } 
-}
-
-
-function getBannedPlayersJSON(){
-    try {
-        if(!fs.existsSync(consts.serverBannedPlayersPath)){
-            return {};
-        }
-        var bannedPlayersJSON = JSON.parse(fs.readFileSync(consts.serverBannedPlayersPath,{ encoding: 'utf8', flag: 'r' }));
-        return bannedPlayersJSON;
-
-    } catch (error) {
-        console.error(error);
-    }  
-}
-
 
 async function updateProperty(propertyName, toggleable, updateValue){
     try{
@@ -234,9 +109,4 @@ module.exports = {
     getProperties,
     updateProperty,
     getOnlinePlayers,
-    getWhitelistJSON,
-    getOpsJSON,
-    getBannedPlayersJSON,
-    modifyOpsJSON,
-    getUUID,
 }
