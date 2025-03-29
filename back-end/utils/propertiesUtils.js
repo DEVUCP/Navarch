8e1c84fc59e4ba71c7ae61717f9004af3956c239
DEVUCP <OMHN05@protonmail.com>
2025-03-29 13:21:08 +0200

    feat(backend): Create modifyOpsJSON propertiesUtils method

--- back-end/utils/propertiesUtils.js
+++ back-end/utils/propertiesUtils.js
@@ -2,6 +2,7 @@ const consts = require("../consts");
 const fs = require("fs");
 const { getConfigAttribute } = require("./configUtils");
 const { spawn } = require('child_process');
+const crypto = require('crypto');
 
 async function getProperties(){
     try{
@@ -41,6 +42,80 @@ function getOpsJSON(){
     } catch (error) {
         console.error(error);
     }  
+}
+
+async function modifyOpsJSON(playerName, add){
+    var opsJSON = getOpsJSON();
+
+    const uuid = await getUUID(playerName);
+    if(add){
+    opsJSON.push(
+        {
+            "uuid": uuid,
+            "name": playerName,
+            "level": 4,
+            "bypassesPlayerLimit": false
+        }
+    )
+    } else {
+        for(let i = 0; i < opsJSON.length ; i++){
+            if(opsJSON[i].uuid == uuid){
+                opsJSON.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+    fs.writeFileSync(consts.serverOpsPath, JSON.stringify(opsJSON, null, 4), {encoding : 'utf8'});
+
+}
+
+async function getUUID(username) {
+    var uuid = await getMojangUUID(username);
+    console.log(uuid);
+    try {
+        if(uuid === ''){
+            console.log("UUID not found, generating one...")
+            uuid = generateOfflineUUID(username);
+        }
+        return uuid;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function getMojangUUID(username) {
+    try {
+        const playerInfo = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
+        const playerInfoJSON = await playerInfo.json();
+        
+        if(!playerInfoJSON)
+            return "";
+        else
+            return `${playerInfoJSON["id"].slice(0, 8)}-${playerInfoJSON["id"].slice(8, 12)}-${playerInfoJSON["id"].slice(12, 16)}-${playerInfoJSON["id"].slice(16, 20)}-${playerInfoJSON["id"].slice(20)}`;
+        
+    } catch (error) {
+        console.error(error);
+    } 
+}
+
+async function generateOfflineUUID(username) {
+
+    
+    try {
+        const data = `OfflinePlayer:${username}`;
+        const hash = crypto.createHash('md5').update(data, 'utf8').digest();
+        
+        // Convert to UUID format (version 3)
+        hash[6] = (hash[6] & 0x0f) | 0x30; // Set version to 3
+        hash[8] = (hash[8] & 0x3f) | 0x80; // Set variant to RFC 4122
+        
+        const hex = hash.toString('hex');
+
+        return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`;
+    } catch (error) {
+        console.error(error);
+    } 
 }
 
 
@@ -153,4 +228,6 @@ module.exports = {
     getWhitelistJSON,
     getOpsJSON,
     getBannedPlayersJSON,
+    modifyOpsJSON,
+    getUUID,
 }
