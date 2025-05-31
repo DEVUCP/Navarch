3da90682c1adfb0b78110e3ac429f88f3f706a2d
DEVUCP <omhn05@protonmail.com>
2025-05-31 16:16:05 +0300

    refactor(api): Update `server.js` to reflect last two refactors

--- api/server.js
+++ api/server.js
@@ -1,98 +1,31 @@
 const express = require('express');
-const cors = require('cors');
-const rateLimit = require("express-rate-limit");
+const middleware = require('./middleware/middleware');
+
+const app = express();
+
+app.use(middleware.cors());
+app.use(middleware.limiter)
+app.use(express.json());
+// app.use(middleware.apiKeyValidation);
+
+// Route files
 const adminRoutes = require('./routes/adminRoutes');
 const configUtils = require('./utils/configUtils')
 const serverRoutes = require('./routes/serverRoutes'); // Routes are separated
 const propertiesRoute = require('./routes/propertiesRoutes'); // Routes are separated
 const installationsRoutes = require('./routes/installationsRoutes'); // Routes are separated
+
+// Util files
 const networkingUtils = require('./utils/networkingUtils');
 const apiAccessUtils = require('./utils/apiAccessUtils');
+const cleaner = require('./cleaner');
 
 // constants
 
-const app = express();
 const port = 3001;
 const debug = configUtils.getConfigAttribute("debug");
 
 
-let cleanup_done = false;
-
-
-async function cleanup() {
-
-    if(debug){
-        console.log("\nDebug mode is turned on\n Skipping cleanup tasks...\n if you don't intend this, change 'debug' from true to false in server-config.json");
-        console.log("====== API TERMINATED! ======");
-        cleanup_done = true;
-        return;
-    }
-
-    console.log('\nRunning cleanup tasks...');
-
-    console.log('Removing port mapping ...');
-    await networkingUtils.removePortMapping(3000);
-
-    console.log('Removing port mapping ...');
-    await networkingUtils.removePortMapping(3001);
-
-    console.log('Removing port mapping ...');
-    await networkingUtils.removePortMapping(configUtils.getConfigAttribute("port"));
-}
-
-process.on('exit', () => {
-    if (!cleanup_done) {
-        cleanup();
-    }
-});
-
-process.on('SIGINT', async () => {
-    await cleanup();
-    process.exit(0);
-});
-
-process.on('SIGTERM', async () => {
-    await cleanup();
-    process.exit(0);
-});
-
-process.on('uncaughtException', async (err) => {
-    console.error('Uncaught Exception:', err);
-    await cleanup();
-    process.exit(1);
-});
-
-
-// Middleware
-
-const apiKeyValidation = (req, res, next) => {
-    const apiKey = req.header('x-api-key');
-    const apiName = req.header('x-api-name');
-    if ((apiKey && 
-        apiAccessUtils.doesEntryExist(apiName, apiKey) 
-        )
-        || debug === true) {
-        console.log("API Key Validation Passed for", req.ip);
-        next();
-    } else {
-        console.log("API Key Validation Failed for", req.ip);
-        res.status(401).json({ error: 'Unauthorized' });
-    }
-  };
-
-const limiter = rateLimit({
-    max: 10, // maximum of 10 requests per window (15 sec) 
-    windowMs:  15 * 1000, // Window : 15 seconds
-    message: "You are being rate-limited.",
-    headers: {
-        "Retry-After": 15
-    }
-});
-
-app.use(limiter)
-app.use(cors());
-app.use(express.json());
-app.use(apiKeyValidation);
 
 // Routes
 app.use('/server', serverRoutes);
