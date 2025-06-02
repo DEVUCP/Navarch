f2464ae79f0615a5fe7aad98f3940c422f1eb3f8
DEVUCP <omhn05@protonmail.com>
2025-06-03 01:59:36 +0300

    fix(scripts): Correct Java SDK version for latest mc (temp fix)

--- scripts/linux_debian/install_dependencies.sh
+++ scripts/linux_debian/install_dependencies.sh
@@ -24,20 +24,38 @@ else
     echo "NPM has been installed"
 fi
 
-# Check Java (OpenJDK 17+)
+# Check Java (OpenJDK 21+)
 if command -v java >/dev/null 2>&1; then
     JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
     JAVA_MAJOR=$(echo $JAVA_VERSION | cut -d. -f1)
-    if [ "$JAVA_MAJOR" -ge 17 ]; then
-        echo "OpenJDK 17 or above is installed"
+    if [ "$JAVA_MAJOR" -ge 21 ]; then
+        echo "OpenJDK 21 or above is installed"
         java -version
     else
-        echo "Java version is less than 17. Installing OpenJDK 17..."
-        sudo apt-get install -y openjdk-17-jdk
-        echo "OpenJDK 17 has been installed"
+        echo "Java version is less than 21. Installing OpenJDK 21..."
+        sudo apt-get install -y openjdk-21-jdk
+        echo "OpenJDK 21 has been installed"
     fi
 else
-    echo "Java is not installed. Installing OpenJDK 17..."
-    sudo apt-get install -y openjdk-17-jdk
-    echo "OpenJDK 17 has been installed"
+    echo "Java is not installed. Installing OpenJDK 21..."
+    sudo apt-get install -y openjdk-21-jdk
+    echo "OpenJDK 21 has been installed"
 fi
+
+# # Check Java (OpenJDK 17+)
+# if command -v java >/dev/null 2>&1; then
+#     JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+#     JAVA_MAJOR=$(echo $JAVA_VERSION | cut -d. -f1)
+#     if [ "$JAVA_MAJOR" -ge 17 ]; then
+#         echo "OpenJDK 17 or above is installed"
+#         java -version
+#     else
+#         echo "Java version is less than 17. Installing OpenJDK 17..."
+#         sudo apt-get install -y openjdk-17-jdk
+#         echo "OpenJDK 17 has been installed"
+#     fi
+# else
+#     echo "Java is not installed. Installing OpenJDK 17..."
+#     sudo apt-get install -y openjdk-17-jdk
+#     echo "OpenJDK 17 has been installed"
+# fi
--- scripts/windows/install_dependencies.bat
+++ scripts/windows/install_dependencies.bat
@@ -29,34 +29,66 @@ if %ERRORLEVEL% EQU 0 (
     echo NPM has been installed
 )
 
-:: Check OpenJDK 17
+:: Check OpenJDK 21
 where java >nul 2>&1
 if %ERRORLEVEL% EQU 0 (
     for /f "tokens=3" %%i in ('java -version 2^>^&1 ^| findstr /i "version"') do set JAVA_VERSION=%%i
     set JAVA_VERSION=%JAVA_VERSION:"=%
     for /f "delims=. tokens=1" %%a in ("%JAVA_VERSION%") do set JAVA_MAJOR=%%a
-    if !JAVA_MAJOR! GEQ 17 (
-        echo OpenJDK 17 or above is installed
+    if !JAVA_MAJOR! GEQ 21 (
+        echo OpenJDK 21 or above is installed
         java -version
     ) else (
-        echo OpenJDK 17 or above is not installed. Installing...
-        curl -o jdk17.zip https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip
-        powershell.exe -Command "Expand-Archive -Path jdk17.zip -DestinationPath 'C:\Program Files\Java'"
-        del jdk17.zip
+        echo OpenJDK 21 or above is not installed. Installing...
+        curl -o jdk21.zip https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_windows-x64_bin.zip
+        powershell.exe -Command "Expand-Archive -Path jdk21.zip -DestinationPath 'C:\Program Files\Java'"
+        del jdk21.zip
         :: Set JAVA_HOME environment variable
-        setx JAVA_HOME "C:\Program Files\Java\jdk-17.0.2" /M
+        setx JAVA_HOME "C:\Program Files\Java\jdk-21.0.1" /M
         :: Add to PATH
         setx PATH "%PATH%;%JAVA_HOME%\bin" /M
-        echo OpenJDK 17 has been installed
+        echo OpenJDK 21 has been installed
     )
 ) else (
     echo Java is not installed. Installing...
-    curl -o jdk17.zip https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip
-    powershell.exe -Command "Expand-Archive -Path jdk17.zip -DestinationPath 'C:\Program Files\Java'"
-    del jdk17.zip
+    curl -o jdk21.zip https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_windows-x64_bin.zip
+    powershell.exe -Command "Expand-Archive -Path jdk21.zip -DestinationPath 'C:\Program Files\Java'"
+    del jdk21.zip
     :: Set JAVA_HOME environment variable
-    setx JAVA_HOME "C:\Program Files\Java\jdk-17.0.2" /M
+    setx JAVA_HOME "C:\Program Files\Java\jdk-21.0.1" /M
     :: Add to PATH
     setx PATH "%PATH%;%JAVA_HOME%\bin" /M
-    echo OpenJDK 17 has been installed
+    echo OpenJDK 21 has been installed
 )
+
+@REM :: Check OpenJDK 17
+@REM where java >nul 2>&1
+@REM if %ERRORLEVEL% EQU 0 (
+@REM     for /f "tokens=3" %%i in ('java -version 2^>^&1 ^| findstr /i "version"') do set JAVA_VERSION=%%i
+@REM     set JAVA_VERSION=%JAVA_VERSION:"=%
+@REM     for /f "delims=. tokens=1" %%a in ("%JAVA_VERSION%") do set JAVA_MAJOR=%%a
+@REM     if !JAVA_MAJOR! GEQ 17 (
+@REM         echo OpenJDK 17 or above is installed
+@REM         java -version
+@REM     ) else (
+@REM         echo OpenJDK 17 or above is not installed. Installing...
+@REM         curl -o jdk17.zip https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip
+@REM         powershell.exe -Command "Expand-Archive -Path jdk17.zip -DestinationPath 'C:\Program Files\Java'"
+@REM         del jdk17.zip
+@REM         :: Set JAVA_HOME environment variable
+@REM         setx JAVA_HOME "C:\Program Files\Java\jdk-17.0.2" /M
+@REM         :: Add to PATH
+@REM         setx PATH "%PATH%;%JAVA_HOME%\bin" /M
+@REM         echo OpenJDK 17 has been installed
+@REM     )
+@REM ) else (
+@REM     echo Java is not installed. Installing...
+@REM     curl -o jdk17.zip https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_windows-x64_bin.zip
+@REM     powershell.exe -Command "Expand-Archive -Path jdk17.zip -DestinationPath 'C:\Program Files\Java'"
+@REM     del jdk17.zip
+@REM     :: Set JAVA_HOME environment variable
+@REM     setx JAVA_HOME "C:\Program Files\Java\jdk-17.0.2" /M
+@REM     :: Add to PATH
+@REM     setx PATH "%PATH%;%JAVA_HOME%\bin" /M
+@REM     echo OpenJDK 17 has been installed
+@REM )
