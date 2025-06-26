415f254c3898f284ddbb4cedc911c6f9c467cfce
OmarSherif06 <omar.sherif.nabawy@gmail.com>
2025-06-26 22:08:06 +0300

    refactor(api): Full properties routes refactor

--- api/routes/propertiesRoutes.js
+++ api/routes/propertiesRoutes.js
@@ -1,179 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const propertiesUtils = require('../utils/propertiesUtils');
-const configUtils = require("../utils/configUtils");
-const jsonFilesUtils = require("../utils/jsonFilesUtils");
+const propertiesServices = require('../services/properties.service');
 
-
-const { Sema } = require('async-sema');
-
-let togglePropertySema = new Sema(1);
-let ramAllocationSema = new Sema(1);
-
+const { 
+    toggleProperty,
+    allocateRam,
+    playerCount,
+    serverConfig,
+    getWhitelist,
+    getOps,
+    getBannedPlayers,
+    modifyOperator,
+    modifyWhitelist,
+    modifyBanned,
+    modifyBannedIPs
+ } = require("../controllers/properties.controller");
 
 router.get('/', async (req, res) => {
-    try{
-        const properties = await propertiesUtils.getProperties();
+    try {
+        const properties = await propertiesServices.getProperties();
         res.status(200).contentType("json").send(properties);
-    }catch(error){
-        console.error(error)
+    } catch(error) {
+        console.error(error);
         res.status(500).send("error.. " + error.message);
     }
 })
 
+router.put('/toggle/:property', toggleProperty);
 
-router.put('/toggle/:property', async (req, res) => {
-    togglePropertySema.acquire()
-    try{
-        console.log(req.params.property);
-        await propertiesUtils.updateProperty(req.params.property, true);
-        res.status(200).send("done");
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }finally{
-        togglePropertySema.release()
-    }
-})
+router.put('/allocate-ram/:mb', allocateRam);
 
+router.get('/player-count', playerCount);
 
-router.put('/allocate-ram/:mb', async (req, res) => {
-    ramAllocationSema.acquire()
-    try{
-        configUtils.updateMemoryAllocated(req.params.mb, true);
-        res.status(200).send(`Ram allocation updated to ${req.params.mb}M`);
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }finally{
-        ramAllocationSema.release()
-    }
-})
+router.get('/server-config.json', serverConfig);
 
-router.get('/player-count', async (req, res) => {
-    try{
-        const playerCount = await propertiesUtils.getOnlinePlayers()
-        res.status(200).send({playerCount: playerCount});
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
+router.get('/whitelist.json', getWhitelist);
 
-router.get('/server-config.json', async (req, res) => {
-    try{
-        const configJSON = configUtils.getConfigJSON();
-        res.status(200).send(configJSON);
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
-router.get('/whitelist.json', async (req, res) => {
-    try{
-        const whitelistJSON = propertiesUtils.getWhitelistJSON();
-        res.status(200).send(whitelistJSON);
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
-router.get('/ops.json', async (req, res) => {
-    try{
-        const opsJSON = propertiesUtils.getOpsJSON();
-        res.status(200).send(opsJSON);
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
-router.get('/banned-players.json', async (req, res) => {
-    try{
-        const bannedplayersJSON = propertiesUtils.getBannedPlayersJSON();
-        res.status(200).send(bannedplayersJSON);
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
-router.put('/:operation/op/:playername', async (req, res) => {
-    try{
-        if(req.params.operation === "add"){
-            await jsonFilesUtils.modifyOpsJSON(req.params.playername, true);
-            res.status(200).send(`Added ${req.params.playername} as an Operator`);
+router.get('/ops.json', getOps);
 
-        } else if(req.params.operation === "remove") {
-            await jsonFilesUtils.modifyOpsJSON(req.params.playername, false);
-            res.status(200).send(`Remove ${req.params.playername} as an Operator`);
+router.get('/banned-players.json', getBannedPlayers);
 
-        } else {
-            res.status(404).send(`Invalid operation ${req.params.operation}`);
-        }
+router.put('/:operation/op/:playername', modifyOperator);
 
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
+router.put('/:operation/whitelist/:playername', modifyWhitelist);
 
-router.put('/:operation/whitelist/:playername', async (req, res) => {
-    try{
-        if(req.params.operation === "add"){
-            await jsonFilesUtils.modifyWhitelistJSON(req.params.playername, true);
-            res.status(200).send(`Added ${req.params.playername} as an Operator`);
+router.put('/:operation/ban/:playername', modifyBanned);
 
-        } else if(req.params.operation === "remove") {
-            await jsonFilesUtils.modifyWhitelistJSON(req.params.playername, false);
-            res.status(200).send(`Remove ${req.params.playername} as an Operator`);
-
-        } else {
-            res.status(404).send(`Invalid operation ${req.params.operation}`);
-        }
-
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
-
-router.put('/:operation/ban/:playername', async (req, res) => {
-    try{
-        if(req.params.operation === "add"){
-            await jsonFilesUtils.modifyBannedPlayersJSON(req.params.playername, true);
-            res.status(200).send(`Banned ${req.params.playername} `);
-
-        } else if(req.params.operation === "remove") {
-            await jsonFilesUtils.modifyBannedPlayersJSON(req.params.playername, false);
-            res.status(200).send(`Pardoned ${req.params.playername}`);
-
-        } else {
-            res.status(404).send(`Invalid operation ${req.params.operation}`);
-        }
-
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
-
-router.put('/:operation/ban-ip/:ip', async (req, res) => {
-    try{
-        if(req.params.operation === "add"){
-            await jsonFilesUtils.modifyBannedIPsJSON(req.params.ip, true);
-            res.status(200).send(`Banned ${req.params.ip} `);
-
-        } else if(req.params.operation === "remove") {
-            await jsonFilesUtils.modifyBannedIPsJSON(req.params.ip, false);
-            res.status(200).send(`Pardoned ${req.params.ip}`);
-
-        } else {
-            res.status(404).send(`Invalid operation ${req.params.operation}`);
-        }
-
-    }catch(error){
-        console.error(error)
-        res.status(500).send("error.. " + error.message);
-    }
-})
+router.put('/:operation/ban-ip/:ip', modifyBannedIPs);
 
 module.exports = router;
